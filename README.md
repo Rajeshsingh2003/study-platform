624f9832795eb2f96c81a422e5faebe92999d4d5
Rajeshsingh2003 <rajeshgill634@gmail.com>
2026-06-18 12:20:00 +0530

    my commit message

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 minikube addons enable ingress
 minikube tunnel..
+minikube service frontend-service -n studyvault --url
 then this will run the pipeline
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,7 @@
 require("dotenv").config();
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+console.log("MONGO_URI =", process.env.MONGO_URI);
 
 const express = require("express");
 const mongoose = require("mongoose");
